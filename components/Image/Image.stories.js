0b21f6e9618a10cfc27d892b2467e5d2f2af93aa
Daniil <Popov>
2021-02-01 23:12:50 +0500

    Update Image

--- components/Image/Image.stories.js
+++ components/Image/Image.stories.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
+import ImagePreview from '../../assets/images/preview-project.png'
 import Image from '.'
 
-storiesOf('Components API/Data Display/Image', module).add('Default', () => {
-  return <Image />
-})
+storiesOf('Components API/Data Display/Image', module)
+  .add('Default', () => {
+    return <Image src={ImagePreview} width={480} />
+  })
+  .add('Magnify', () => {
+    return <Image src={ImagePreview} width={480} magnify />
+  })
--- components/Image/index.js
+++ components/Image/index.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-export const Wrap = styled.img``
+export const Figure = styled.figure`
+  background-repeat: no-repeat;
+  cursor: crosshair;
 
-export const Image = ({ ...props }) => <Wrap {...props} />
+  &:hover img {
+    opacity: 0;
+  }
+`
+
+export const IMG = styled.img``
+
+export const Image = ({ src, width, height, magnify, multiply, ...props }) => {
+  const [state, setState] = useState({
+    backgroundImage: `url(${src})`,
+    backgroundPosition: '0% 0%'
+  })
+
+  const handleMouseMove = (e) => {
+    const { left, top, width, height } = e.target.getBoundingClientRect()
+    const x = (e.pageX - left) / width * 100
+    const y = (e.pageY - top) / height * 100
+
+    setState((prev) => ({ ...prev, backgroundSize: `${100 * multiply}%`, backgroundPosition: `${x}% ${y}%` }))
+  }
+
+  const handleMouseLeave = () => {
+    setState((prev) => ({ ...prev, backgroundSize: '100%' }))
+  }
+
+  return (
+    magnify ? (
+      <Figure
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        style={{ ...state, width, height }}
+      >
+        <IMG {...props} src={src} style={{ width, height }} />
+      </Figure>
+    ) : (
+      <IMG {...props} src={src} style={{ width, height }} />
+    )
+  )
+}
+
+Image.defaultProps = {
+  multiply: 3
+}
 
 export default Image
