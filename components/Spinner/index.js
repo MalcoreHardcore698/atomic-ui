743e5005f43e78b20837b3af8deea9966d2119f5
Daniil <Popov>
2021-02-26 13:21:35 +0500

    Relocate keyframes

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -23,15 +23,6 @@ export const styles = {
     borderRadius: '50%',
     width: '3em',
     height: '3em'
-  },
-
-  '@keyframes load': {
-    '0%': {
-      transform: 'rotate(0deg)'
-    },
-    '100%': {
-      transform: 'rotate(360deg)'
-    }
   }
 }
 
